ef57adfd3aea9d72d664588e356d65dfb98781e0
oracledestiny <108359844+MiracleDestiny@users.noreply.github.com>
2024-11-15 21:41:46 +0700

    feat : added authenticate to login

--- kab-kab/src/utils/auth.ts
+++ kab-kab/src/utils/auth.ts
@@ -5,17 +5,20 @@ export async function authenticateLogin({
   userId,
   inputPassword,
 }: {
-  userId: string;
+  userId: number;
   inputPassword: string;
 }) {
   const hashedInput = await bcrypt.hash(inputPassword, SALT_ROUNDS);
   // Load hash from your password DB.
-  // const password = await prisma.user.findFirst({
-  //   where: {
-  //     id: userId,
-  //   },
-  // });
-  return bcrypt.compare(inputPassword, hashedInput); // true
+  const user = await prisma.user.findFirst({
+    where: {
+      id: userId,
+    },
+    select: {
+      password: true,
+    },
+  });
+  return !user ? false : bcrypt.compare(user.password, hashedInput); // true
 }
 
 export async function hashPassword(inputPassword: string) {
